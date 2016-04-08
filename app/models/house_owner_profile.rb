1b938c08d5db2292095ff2ad69842b1987593fd5
anuragscsit <anumfp@gmail.com>
2016-04-08 18:23:23 +0530

    minor changes in tenant dashboard

--- app/models/house_owner_profile.rb
+++ app/models/house_owner_profile.rb
@@ -1,3 +1,6 @@
 class HouseOwnerProfile < ActiveRecord::Base
 	belongs_to :house_owner
+	def full_address
+    	p_address + " " + p_city + " " +p_state
+  	end
 end
--- app/models/tenant_profile.rb
+++ app/models/tenant_profile.rb
@@ -1,6 +1,6 @@
 class TenantProfile < ActiveRecord::Base
 	belongs_to :tenant
 	def full_address
-		p_address + " " + p_city
-	end
+    	p_address + " " + p_city + " " +p_state
+  	end
 end
