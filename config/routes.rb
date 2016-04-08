26d8999c19420c20359096eba138a181337e9b87
anuragscsit <anumfp@gmail.com>
2016-04-09 02:44:14 +0530

    added owner page and improve design

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get 'tenants/profile'
 
   get 'home/index'
+  get 'home/owner'
 
   devise_for :tenants, skip: [:sessions]
   devise_for :house_owners, skip: [:sessions]
