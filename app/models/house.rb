d9d266d4fcbc117c5f323878647ec1ddccada82e
anuragscsit <anumfp@gmail.com>
2016-04-27 15:14:38 +0530

    fixed requied field for house

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,5 +1,6 @@
 class House < ActiveRecord::Base
 	belongs_to :house_owner
 
+	validates :house_image, :city, :location, :latitude, :longitude, presence: true
 	mount_uploaders :house_image, ImageUploader
 end
