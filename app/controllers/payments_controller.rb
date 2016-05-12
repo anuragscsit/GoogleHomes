b55531e42530ff5fe00fc82dcc7bbd2a2a362f13
anuragscsit <anumfp@gmail.com>
2016-05-13 01:41:49 +0530

    added house booking module

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,11 +1,17 @@
 class PaymentsController < ApplicationController
 	before_action :auth_tenant
-  def auth_tenant
-    redirect_to  new_tenant_registration_url unless user_signed_in?
-  end
+	def auth_tenant
+	    redirect_to  new_tenant_registration_url unless user_signed_in?
+	end
 
 	def bed_confirmation
 		house = params[:house_id]
 		@house = House.find(house)
+	end
+	def confirmation
+		house = params[:house_id]
+		@house = House.find(house)
+		@tenant= current_user
+		@move_in_date = params[:date]
 	end
 end
